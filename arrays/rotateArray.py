12e19821ac92550a86e42d4551eb87c5931c08a3
iharshcheema <harshcheemaweb@gmail.com>
2024-11-29 23:31:14 +0530

    comments

--- arrays/rotateArray.py
+++ arrays/rotateArray.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def reverse(self, array, start, end ):
-        # phla element ek index age jara aur last element ek index piche ara to phla element surpass na krde 
+        # phla element ek index age jara aur last element ek index piche ara to phla element surpass na krde uske liye while loop 
         while(start<end):
             array[start], array[end] = array[end], array[start]
             start += 1
@@ -22,3 +22,30 @@ class Solution(object):
         # [3,4,5,2,1]
         self.reverse(array , k , len(array)-1 )
         [3,4,5,1,2]
+
+# Why are we using self in your code?
+# In your Solution class:
+
+# self.reverse:
+# This refers to the reverse method defined in the Solution class.
+# By writing self.reverse(array, 0, len(array) - 1), you are calling the reverse method of the same instance.
+# Without self, Python would not know where to find the reverse method, because it assumes functions without self are either local to the method or globally defined.
+
+
+
+
+# How the Code Works with self:
+# You define a class Solution with two methods:
+
+# reverse to reverse a portion of the array in-place.
+# rotate to rotate the array by k steps.
+# Inside rotate, you call the reverse method using self.reverse to reverse parts of the array in sequence.
+
+# For example, in this line:
+
+# self.reverse(array, 0, len(array) - 1)
+# self ensures that Python looks for reverse in the Solution instance
+
+
+# Time complexity 
+# The time complexity of the rotate function using the reverse method is O(n)
