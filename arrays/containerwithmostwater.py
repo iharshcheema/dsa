a0c5da80f1502f03eedd91f73ac9c18e99d75134
iharshcheema <harshcheemaweb@gmail.com>
2024-12-02 23:57:24 +0530

    2nd method to solve container with most water

--- arrays/containerwithmostwater.py
+++ arrays/containerwithmostwater.py
@@ -29,6 +29,33 @@ class Solution(object):
 
 
 # Method 2 
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1  # Initialize pointers
+        max_area = 0  # Variable to track the maximum area
+
+        while left < right:
+            # Calculate the area
+            width = right - left
+            min_height = min(height[left], height[right])
+            area = width * min_height
+            
+            # Update the maximum area
+            max_area = max(max_area, area)
+            
+            # Move the pointers
+            if height[left] < height[right]:
+                left += 1  # Move left pointer inward
+            else:
+                right -= 1  # Move right pointer inward
+
+        return max_area
+
+
 
 
 
